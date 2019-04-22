9e31c46564b10e0c378280e402c35da2a2006d86
AlexandrTyurikov <tyur.sh@gmail.com>
2019-04-22 06:41:37 +0300

    search

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, ListView, TemplateView, CreateView
+from django.views.generic import DetailView, ListView, TemplateView
 from .models import *
 
 
@@ -26,27 +26,57 @@ class ProductsDetail(DetailView):
 class AuthorList(ListView):
     model = Author
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('aaa', 0)
+        if search != 0:
+            return qs.filter(name__contains=search)
+        return qs
+
 
 class SeriesList(ListView):
     model = Series
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('sss', 0)
+        if search != 0:
+            return qs.filter(series__contains=search)
+        return qs
 
 
 class GenreList(ListView):
     model = Genre
 
-
-class ProductsList(ListView):
-    model = Products
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('ggg', 0)
+        if search != 0:
+            return qs.filter(genre__contains=search)
+        return qs
 
 
 class PublishingList(ListView):
     model = Publishing
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('ppp', 0)
+        if search != 0:
+            return qs.filter(publishing__contains=search)
+        return qs
+
+
+class ProductsList(ListView):
+    model = Products
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        search = self.request.GET.get('bbb', 0)
+        if search != 0:
+            return qs.filter(name__contains=search)
+        return qs
+
 
 class AllHandbookView(TemplateView):
     template_name = 'products/all_handbook_view.html'
-
-
-class AuthorCreate(CreateView):
-    model = Author
-    fields = ['name']
